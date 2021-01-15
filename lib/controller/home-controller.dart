189c2143a2623282ad85b6a1754cf095611c5de5
lucas <lucas.cardo123@hotmail.com>
2021-01-14 22:32:00 -0400

    ajustes de no metodo getDrink, parse com lista de Objetos

--- lib/controller/home-controller.dart
+++ lib/controller/home-controller.dart
@@ -2,14 +2,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:cocktail_app/model/drink-model.dart';
 
-Future<Drink> getDrink({String filtro}) async {
+Future<List<Drink>> getDrink({String filtro}) async {
   final response = await http
       .get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?$filtro');
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    return Drink.fromJson(jsonDecode(response.body));
+    List<dynamic> records = await jsonDecode(response.body)['drinks'];
+
+    return records.map((e) => Drink.fromJson(e)).toList();
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
